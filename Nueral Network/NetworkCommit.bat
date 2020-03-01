3e0236f983a1fc0cd1d12979f3aaae19b3fd3cc3
kyle <kyle@localhost>
2020-03-01 16:14:05 -0700

    hi

--- Nueral Network/NetworkCommit.bat
+++
@@ -1,4 +0,0 @@
-CHDIR "C:\Users\JackPaul\git\Digit-Processing-Neural-Network-1"
-GIT add "Nueral Network\MyFirstNetwork"
-GIT commit -m "Windows Auto Commit"
-GIT push
--- Nueral Network/commitNetwork.sh
+++ Nueral Network/commitNetwork.sh
@@ -1,4 +1,4 @@
 cd /Users/kyle/git/Digit-Processing
-git add "Nueral Network/MyFirstNetwork"
-git commit -m "auto-commit"
+git add "Nueral Network/KylesNetwork"
+git commit -m "Kyle's auto-commit"
 git push
